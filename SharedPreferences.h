d0b7add1a7b2d8a644dddd48eb19cea76f652823
Chris Sells <csells@sellsbrothers.com>
2019-07-31 01:04:03 -0700

    updated for most functionality
    still have to handle getKeys for StringList

--- SharedPreferences.h
+++ SharedPreferences.h
@@ -1,14 +1,17 @@
 #pragma once
-
 #include "pch.h"
 #include <sstream>
+#include <vector>
+#include <variant>
 #include "basic_debugbuf.h"
 
 using namespace std;
 
 class SharedPreferences {
-  const wstring prefsFileName = getPrefsFileName();
   const wchar_t* sectionName = L"preferences";
+  const wchar_t* containsUuid = L"f042b00f-adcd-4462-94ba-fae5265a541c";
+  const int MAX_VALUE_LENGTH = 1024;
+  const wstring prefsFileName = getPrefsFileName();
 
 public:
   SharedPreferences() {
@@ -19,6 +22,7 @@ private:
   static wstring getPrefsFileName() {
     wostringstream ss;
 
+    // TODO: update this to use Windows resources, i.e. company name and app name
     PWSTR localAppDataFolder = NULL;
     SHGetKnownFolderPath(FOLDERID_LocalAppData, KF_FLAG_CREATE, NULL, &localAppDataFolder);
     ss << localAppDataFolder;
@@ -61,9 +65,60 @@ private:
     return ss.str();
   }
 
+  static wstring getListSizeKey(const wchar_t* key) {
+    return (wostringstream() << key << L".size").str();
+  }
+
+  static wstring getListIndexKey(const wchar_t* key, int index) {
+    return (wostringstream() << key << L"[" << index << L"]").str();
+  }
+
 public:
+  wstring getString(const wchar_t* key, const wchar_t* defval = 0) {
+    wstring value;
+    value.reserve(MAX_VALUE_LENGTH);
+
+    auto size = GetPrivateProfileStringW(sectionName, key, defval, &value[0], value.capacity(), prefsFileName.c_str());
+    if (size == 0) {
+      wostringstream err;
+      err << L"getString failed. key= '" << key << L"'. " << getLastErrorMessage();
+      throw err.str();
+    }
+
+    return value;
+  }
+
+  bool getBool(const wchar_t* key, bool defval = false) {
+    return stoi(getString(key, to_wstring(defval).c_str())) != 0;
+  }
+
+  int getInt(const wchar_t* key, int defval = 0) {
+    return stoi(getString(key, to_wstring(defval).c_str()));
+  }
+
+  double getDouble(const wchar_t* key, double defval = 0.0) {
+    return stod(getString(key, to_wstring(defval).c_str()));
+  }
+
+  vector<wstring> getStringList(const wchar_t* key, vector<const wchar_t*> defvals = vector<const wchar_t*>()) {
+    vector<wstring> values;
+
+    int size = getInt(getListSizeKey(key).c_str());
+    if (size == 0) {
+      for (auto pdefval = defvals.begin(); pdefval != defvals.end(); ++pdefval) {
+        values.push_back(*pdefval);
+      }
+    }
+    else {
+      for (auto i = 0; i != size; ++i) {
+        values.push_back(getString(getListIndexKey(key, i).c_str()));
+      }
+    }
+
+    return values;
+  }
+
   void setString(const wchar_t* key, const wchar_t* value) {
-    // TODO: write this to an ini file at the LocalAppData folder
     auto success = WritePrivateProfileStringW(sectionName, key, value, prefsFileName.c_str());
     if (!success) {
       wostringstream err;
@@ -72,17 +127,70 @@ public:
     }
   }
 
-  wstring getString(const wchar_t* key, const wchar_t* defaultValue = NULL) {
-    wstring value;
-    value.reserve(256);
-    auto success = GetPrivateProfileStringW(sectionName, key, defaultValue, &value[0], value.capacity(), prefsFileName.c_str());
-    if (!success) {
-      wostringstream err;
-      err << L"getString failed. key= '" << key << L"', defaultValue= '" << defaultValue << L"'. " << getLastErrorMessage();
-      throw err.str();
-    }
-
-    return value;
+  void setBool(const wchar_t* key, bool value) {
+    setString(key, to_wstring(value).c_str());
   }
 
+  void setInt(const wchar_t* key, int value) {
+    setString(key, to_wstring(value).c_str());
+  }
+
+  void setDouble(const wchar_t* key, double value) {
+    setString(key, to_wstring(value).c_str());
+  }
+
+  void setStringList(const wchar_t* key, vector<const wchar_t*> values) {
+    setInt(getListSizeKey(key).c_str(), values.size());
+    for (auto i = 0; i != values.size(); ++i) {
+      setString(getListIndexKey(key, i).c_str(), values[i]);
+    }
+  }
+
+  void remove(const wchar_t* key) {
+    // check if we're removing a list
+    wstring listSizeKey = getListSizeKey(key);
+    auto size = getInt(listSizeKey.c_str());
+    if (size != 0) {
+      setString(listSizeKey.c_str(), 0);
+      for (auto i = 0; i != size; i++) {
+        setString(getListIndexKey(key, i).c_str(), 0);
+      }
+    }
+    else {
+      setString(key, 0);
+    }
+  }
+
+  bool containsKey(const wchar_t* key) {
+    // if our default UUID is returned to us, the key isn't set in our preferences
+    return wcscmp(getString(key, containsUuid).c_str(), containsUuid) != 0;
+  }
+
+  vector<wstring> getKeys() {
+    vector<wstring> keys;
+
+    wstring section;
+    section.reserve(32767);
+    auto size = GetPrivateProfileSectionW(sectionName, &section[0], section.capacity(), prefsFileName.c_str());
+    if (size != 0) {
+      // traverse double-null terminated string
+      for (const wchar_t* pszz = &section[0]; *pszz; pszz += lstrlen(pszz) + 1) {
+        auto pszzend = pszz + lstrlen(pszz);
+        auto peq = find(pszz, pszzend, L'=');
+        auto key = wstring(pszz, peq);
+        keys.push_back(key);
+      }
+    }
+
+    return keys;
+  }
+
+  void clear() {
+    auto success = WritePrivateProfileSectionW(sectionName, 0, prefsFileName.c_str());
+    if (!success) {
+      wostringstream err;
+      err << L"clear failed. " << getLastErrorMessage();
+      throw err.str();
+    }
+  }
 };
--- shared_prefs_proto.cpp
+++ shared_prefs_proto.cpp
@@ -3,13 +3,17 @@
 #include "SharedPreferences.h"
 #include <iostream>
 #include <sstream>
+#include "shared_prefs_proto.h"
 
-#define E(e, v) expect(TEXT(__FILE__), __LINE__, e, v)
+#define E(e, a) expect(TEXT(__FILE__), __LINE__, e, a)
 
-void expect(wstring file, int line, const wchar_t* expected, const wchar_t* actual) {
+template<class T> bool areEqual(T lhs, T rhs) { return lhs == rhs; }
+template<> bool areEqual(const wchar_t* lhs, const wchar_t* rhs) { return wcscmp(lhs, rhs) == 0; }
+
+template<class T> void expect(wstring file, int line, T expected, T actual) {
   wostringstream fileline;
-  fileline << file.substr(file.find_last_of(L'\\')+1) << "(" << line << ")";
-  if (wcscmp(expected, actual) == 0) {
+  fileline << file.substr(file.find_last_of(L'\\') + 1) << "(" << line << ")";
+  if (areEqual<T>(expected, actual)) {
     wcout << "test passed @ " << fileline.str() << endl;
   }
   else {
@@ -17,11 +21,76 @@ void expect(wstring file, int line, const wchar_t* expected, const wchar_t* actu
   }
 }
 
+// from https://github.com/flutter/plugins/blob/master/packages/shared_preferences/test/shared_preferences_test.dart
 int main() {
   auto prefs = new SharedPreferences();
 
-  // from https://github.com/flutter/plugins/blob/master/packages/shared_preferences/test/shared_preferences_test.dart
+  // set some values
   prefs->setString(L"s1", L"hello, world");
-  auto value = prefs->getString(L"s1");
-  E(L"hello, world", value.c_str());
+  prefs->setBool(L"b1", true);
+  prefs->setInt(L"i1", 42);
+  prefs->setDouble(L"d1", 3.14);
+
+  auto sl1a = vector<const wchar_t*>();
+  sl1a.push_back(L"foo");
+  sl1a.push_back(L"bar");
+  prefs->setStringList(L"sl1", sl1a);
+
+  // get some values
+  auto s1 = prefs->getString(L"s1");
+  E(L"hello, world", s1.c_str());
+
+  auto b1 = prefs->getBool(L"b1");
+  E(true, b1);
+
+  auto i1 = prefs->getInt(L"i1");
+  E(42, i1);
+
+  auto d1 = prefs->getDouble(L"d1");
+  E(3.14, d1);
+
+  auto sl1b = prefs->getStringList(L"sl1");
+  E(sl1a.size(), sl1b.size());
+  for (auto i = 0; i != sl1a.size(); ++i) {
+    E(sl1a[i], sl1b[i].c_str());
+  }
+
+  // get the list of keys
+  vector<wstring> keys = prefs->getKeys();
+  E(true, find(keys.begin(), keys.end(), L"s1") != keys.end());
+  E(true, find(keys.begin(), keys.end(), L"b1") != keys.end());
+  E(true, find(keys.begin(), keys.end(), L"i1") != keys.end());
+  E(true, find(keys.begin(), keys.end(), L"d1") != keys.end());
+  E(true, find(keys.begin(), keys.end(), L"sl1") != keys.end()); // TODO: make this work
+
+  // remove some values
+  prefs->remove(L"s1");
+  prefs->remove(L"b1");
+  prefs->remove(L"i1");
+  prefs->remove(L"d1");
+  prefs->remove(L"sl1");
+
+  // get some default values
+  auto s1b = prefs->getString(L"s1", L"goodnight moon");
+  E(L"goodnight moon", s1b.c_str());
+
+  auto b1b = prefs->getBool(L"d1");
+  E(false, b1b);
+
+  auto i1b = prefs->getInt(L"i1");
+  E(0, i1b);
+
+  auto d1b = prefs->getDouble(L"d1");
+  E(0.0, d1b);
+
+  auto sl1c = prefs->getStringList(L"sl1");
+  E((size_t)0, sl1c.size());
+
+  auto containsKey = prefs->containsKey(L"key1");
+  E(false, containsKey);
+
+  prefs->setString(L"foo", L"bar");
+  prefs->clear();
+  auto containsKey2 = prefs->containsKey(L"foo");
+  E(false, containsKey2);
 }
